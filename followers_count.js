7ba09e969532a8f666cda53e1f93cd22af4544ef
Yonatankinfe <yonatankinfe0@gmail.com>
2026-01-05 06:04:00 -0500

    message

---
+++ followers_count.js
@@ -0,0 +1,21 @@
+let count = 0;
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function checkCountValue(){
+
+    if(count === 10){
+        alert("your instagram post gained 10 follow");
+
+    }else if(count === 20){
+        alert("your instagram post gained 20 followers");
+    }
+}
+
